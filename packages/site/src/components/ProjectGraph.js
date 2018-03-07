9f43517e7fda1697b644c6cec3b2a03a6bf910f3
willyspinner <wilsonjusuf1998@gmail.com>
2018-03-07 13:53:57 -0800

    updated ProjectGraph

--- packages/site/src/components/ProjectGraph.js
+++ packages/site/src/components/ProjectGraph.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+var Graph = require('react-graph-viz')
 class ProjectGraph extends React.Component {
     constructor(props) {
-        this.state = props
+        super(props);
+        this.state.graph  = {
+            nodes: this.props.nodes,
+            edges: this.props.edges,
+        }
+    }
+
+    var options = {
+        layout: {
+            hierarchical: true
+        },
+        edges: {
+            color: "#000000"
+        }
+    };
+
+    var events = {
+        select: function(event) {
+            var { taskID, edges } = event;
+            // TODO: Do when onclick on the nodes id
+            alert('clicked on taskID: '+taskID);
+
+        }
+
+
     }
 
     componentDidMount() {
     }
+
     componentWillUnmount() {
 
+    }
+    render(){
+        <Graph graph={this.state.graph} options={options} events={events} />
     }
 }
 
