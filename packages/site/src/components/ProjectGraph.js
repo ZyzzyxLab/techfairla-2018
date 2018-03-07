c736e1b29950655ca1d18d90f4b7a78c7cf91b54
willyspinner <wilsonjusuf1998@gmail.com>
2018-03-07 14:08:05 -0800

    updated projectgraph

--- packages/site/src/components/ProjectGraph.js
+++ packages/site/src/components/ProjectGraph.js
@@ -1,45 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-var Graph = require('react-graph-viz')
+import '../index.css';
+var Graph = require('react-graph-vis');
 class ProjectGraph extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state.graph  = {
-            nodes: this.props.nodes,
-            edges: this.props.edges,
-        }
-    }
-
-    var options = {
-        layout: {
-            hierarchical: true
-        },
-        edges: {
-            color: "#000000"
-        }
+  constructor(props) {
+    super(props);
+    this.state.graph = {
+      nodes: this.props.nodes,
+      edges: this.props.edges
     };
+  }
 
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+  render() {
+    var options = {
+      layout: {
+        hierarchical: true
+      },
+      edges: {
+        color: '#000000'
+      }
+    };
     var events = {
-        select: function(event) {
-            var { taskID, edges } = event;
-            // TODO: Do when onclick on the nodes id
-            alert('clicked on taskID: '+taskID);
-
-        }
-
-
-    }
-
-    componentDidMount() {
-    }
-
-    componentWillUnmount() {
-
-    }
-    render(){
-        <Graph graph={this.state.graph} options={options} events={events} />
-    }
+      select: function(event) {
+        var { taskID, edges } = event;
+        // TODO: Do when onclick on the nodes id
+        alert('clicked on taskID: ' + taskID);
+      }
+    };
+    return <Graph graph={this.state.graph} options={options} events={events} />;
+  }
 }
 
 export default ProjectGraph;
--- packages/site/src/views/ProjectView.js
+++ packages/site/src/views/ProjectView.js
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
-import logo from './logo.svg';
-import './App.css';
-import ProjectGraph from './components/ProjectGraph';
+import logo from '../logo.svg';
+import '../App.css';
+import ProjectGraph from '../components/ProjectGraph';
 import { gql } from 'apollo-boost';
 import { Query } from 'react-apollo';
 
 class ProjectView extends Component {
-    const nodes = [
-      {id: 1, label: 'Node 1'},
-      {id: 2, label: 'Node 2'},
-      {id: 3, label: 'Node 3'},
-      {id: 4, label: 'Node 4'},
-      {id: 5, label: 'Node 5'}
-  ];
-    const  edges = [
-      {from: 1, to: 2},
-      {from: 1, to: 3},
-      {from: 2, to: 4},
-      {from: 2, to: 5}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const graphnodes = [
+      { id: 1, label: 'Node 1' },
+      { id: 2, label: 'Node 2' },
+      { id: 3, label: 'Node 3' },
+      { id: 4, label: 'Node 4' },
+      { id: 5, label: 'Node 5' }
     ];
+    const edges = [
+      { from: 1, to: 2 },
+      { from: 1, to: 3 },
+      { from: 2, to: 4 },
+      { from: 2, to: 5 }
+    ];
+    return <ProjectGraph nodes={graphnodes} edges={edges} />;
+  }
+  //TODO: WIRE THIS UP TO DB LATER.
+}
 
-      render() {
-        return (
-            <ProjectGraph nodes=nodes edges=edges />
-        )
-        }
-    }
-
-    export default ProjectView;
+export default ProjectView;
