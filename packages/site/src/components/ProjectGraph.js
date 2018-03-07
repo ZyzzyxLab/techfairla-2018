7a3872c0582b09e61bfbd5b99179d8de0d7c5533
willyspinner <wilsonjusuf1998@gmail.com>
2018-03-07 13:27:33 -0800

    components and views

---
+++ packages/site/src/components/ProjectGraph.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+class ProjectGraph extends React.Component {
+    constructor(props) {
+        this.state = props
+    }
+
+    componentDidMount() {
+    }
+    componentWillUnmount() {
+
+    }
+}
+
+export default ProjectGraph;
