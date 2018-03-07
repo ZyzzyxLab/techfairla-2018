e82beb995eac8286d66788332fe2353d22c58b9c
willyspinner <wilsonjusuf1998@gmail.com>
2018-03-07 14:37:32 -0800

    project graph works

--- packages/site/src/components/ProjectGraph.js
+++ packages/site/src/components/ProjectGraph.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../index.css';
-var Graph = require('react-graph-vis');
+import Graph from 'react-graph-vis';
+//const Graph = require('react-graph-vis').default;
+
 class ProjectGraph extends React.Component {
   constructor(props) {
     super(props);
-    this.state.graph = {
-      nodes: this.props.nodes,
-      edges: this.props.edges
+    this.state = {
+      graph: { nodes: this.props.nodes, edges: this.props.edges }
     };
   }
 
   componentDidMount() {}
 
   componentWillUnmount() {}
+
   render() {
+    const { nodes, edges } = this.props;
+    const graph = { nodes, edges };
     var options = {
       layout: {
         hierarchical: true
@@ -23,11 +27,17 @@ class ProjectGraph extends React.Component {
         color: '#000000'
       }
     };
-    var events = {
+    const events = {
       select: function(event) {
-        var { taskID, edges } = event;
+        var { nodes, edges } = event;
         // TODO: Do when onclick on the nodes id
-        alert('clicked on taskID: ' + taskID);
+        let taskID = nodes;
+        if (taskID != '')
+          alert(
+            'clicked on taskID: ' +
+              taskID +
+              '. TODO: REDIRECT TO THE TASKVIEW IF PUBLIC.'
+          );
       }
     };
     return <Graph graph={this.state.graph} options={options} events={events} />;
--- packages/site/src/views/ProjectView.js
+++ packages/site/src/views/ProjectView.js
@@ -11,20 +11,21 @@ class ProjectView extends Component {
     super(props);
   }
   render() {
-    const graphnodes = [
-      { id: 1, label: 'Node 1' },
-      { id: 2, label: 'Node 2' },
-      { id: 3, label: 'Node 3' },
-      { id: 4, label: 'Node 4' },
-      { id: 5, label: 'Node 5' }
+    const taskNodes = [
+      { id: '1', label: 'Node 1', color: '#e04141' },
+      { id: '2', label: 'Node 2', color: '#e09c41' },
+      { id: '3', label: 'Node 3', color: '#e0df41' },
+      { id: '4', label: 'Node 4', color: '#7be041' },
+      { id: '5', label: 'Node 5', color: '#41e0c9' }
     ];
-    const edges = [
+    //Connections between tasks:
+    const taskEdges = [
       { from: 1, to: 2 },
       { from: 1, to: 3 },
       { from: 2, to: 4 },
       { from: 2, to: 5 }
     ];
-    return <ProjectGraph nodes={graphnodes} edges={edges} />;
+    return <ProjectGraph nodes={taskNodes} edges={taskEdges} />;
   }
   //TODO: WIRE THIS UP TO DB LATER.
 }
